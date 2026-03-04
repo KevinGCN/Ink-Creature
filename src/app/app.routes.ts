b747bc56dece0b1cd49a5d8f475fadd050a46ece
KevinGCN <cobokevingiancarlo1@gmail.com>
2026-03-04 13:46:25 -0500

    First contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Lobby } from './lobby/lobby';
+import { Loggin } from './loggin/loggin';
+import { Gallery } from './gallery/gallery';
+import { Employees } from './employees/employees';
+import { Schedule } from './schedule/schedule';
+import { Information } from './information/information';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Loggin }, // ruta por defecto
+  { path: 'lobby', component: Lobby },
+  { path: 'gallery', component: Gallery },
+  { path: 'employees', component: Employees },
+  { path: 'schedule', component: Schedule },
+  { path: 'information', component: Information },
+  { path: 'profile', component: Profile },
+  { path: '**', redirectTo: '' } // ruta comodín (si no existe)
+];
---
+++ src/app/employees/employees.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  imports: [],
+  standalone: true,
+  templateUrl: './employees.html',
+  styleUrl: './employees.css',
+})
+export class Employees {
+
+}

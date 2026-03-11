1c78a44a2b8e02d94350f28aa59f53d9fa21646e
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-03-11 12:52:15 -0500

    Actualizar loggin

--- src/app/lobby/lobby.ts
+++ src/app/lobby/lobby.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Loggin } from '../loggin/loggin';
 
 @Component({
   selector: 'app-lobby',
-  imports: [],
   standalone: true,
+  imports: [Loggin],
   templateUrl: './lobby.html',
-  styleUrl: './lobby.css',
+  styleUrls: ['./lobby.css']
 })
 export class Lobby {
+
+  @ViewChild('login') login!: Loggin;
+
+  abrirLogin() {
+    this.login.abrir();
+  }
 
 }

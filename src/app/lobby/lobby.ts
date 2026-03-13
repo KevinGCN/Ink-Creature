0f1a41825a521d4fad90d0deb598379eee494b83
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-03-13 02:36:57 -0500

    Login completo y funcional

--- src/app/lobby/lobby.ts
+++ src/app/lobby/lobby.ts
@@ -1,26 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
-import { Loggin } from '../loggin/loggin';
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-lobby',
+  imports: [RouterLink],
   standalone: true,
-  imports: [Loggin],
   templateUrl: './lobby.html',
   styleUrls: ['./lobby.css']
 })
+
 export class Lobby {
-  @ViewChild('login') login!: Loggin;
 
-<<<<<<< HEAD
-  abrirLogin() {
-    this.login.abrir();
-  }
-=======
-  @ViewChild('login') login!: Loggin;
-
-  abrirLogin() {
-    this.login.abrir();
-  }
-
->>>>>>> parent of 0495e67 (Merge branch 'dev-emerson' of https://github.com/KevinGCN/Angular into dev-emerson)
 }

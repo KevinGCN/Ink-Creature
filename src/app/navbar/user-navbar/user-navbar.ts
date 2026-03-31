9fae82ae0ee7d4d4322ac8a5a8219994a310765d
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-03-30 23:02:46 -0500

    Inicio limpio con perfil y login

--- src/app/navbar/user-navbar/user-navbar.ts
+++ src/app/navbar/user-navbar/user-navbar.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Loggin } from '../../loggin/loggin';
 
 @Component({
   selector: 'app-user-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink, Loggin],
   templateUrl: './user-navbar.html',
   styleUrl: './user-navbar.css',
 })
 export class UserNavbar {
+  mostrarLogin = false;
 
+  abrirLogin() {
+    this.mostrarLogin = true;
+  }
+
+  cerrarLogin() {
+    this.mostrarLogin = false;
+  }
 }

cddbebfe69275e1ef02ac17ac97e6640f44d21d1
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-04-01 05:20:58 -0500

    Arreglos completos: navbar, loggin y configuración JSON

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,12 @@
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
+// Inicializar entorno de pruebas de Angular
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);

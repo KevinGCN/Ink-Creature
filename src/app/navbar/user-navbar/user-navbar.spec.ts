e78aea308e2b218958de0ad0a90f63b5f925583d
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-04-01 04:05:33 -0500

    fix RouterTestingModule usage

--- src/app/navbar/user-navbar/user-navbar.spec.ts
+++ src/app/navbar/user-navbar/user-navbar.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserNavbar } from './user-navbar';
 
@@ -6,17 +7,15 @@ describe('UserNavbar', () => {
   let component: UserNavbar;
   let fixture: ComponentFixture<UserNavbar>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UserNavbar]
-    })
-    .compileComponents();
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    imports: [UserNavbar, RouterTestingModule]
+  }).compileComponents();
 
-    fixture = TestBed.createComponent(UserNavbar);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
-
+  fixture = TestBed.createComponent(UserNavbar);
+  component = fixture.componentInstance;
+  fixture.detectChanges(); // renderiza el template
+});
   it('should create', () => {
     expect(component).toBeTruthy();
   });

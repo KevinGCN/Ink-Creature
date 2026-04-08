dfe242e39c58004c161557d467763a96a70b5d66
KevinGCN <cobokevingiancarlo1@gmail.com>
2026-04-08 14:48:22 -0500

    Revert "Dev emerson"

---
+++ src/app/employee-cv/employee-cv.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-cv',
+  imports: [],
+  templateUrl: './employee-cv.html',
+  styleUrl: './employee-cv.css',
+})
+export class EmployeeCV {
+
+}

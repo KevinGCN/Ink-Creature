2eca12afb2307a04ab4cedeeabf1fa432b2a8419
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-04-13 14:47:23 -0500

    avance: auth service y cambios en login y perfil

--- src/app/navbar/user-navbar/user-navbar.ts
+++ src/app/navbar/user-navbar/user-navbar.ts
@@ -1,36 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Loggin } from '../../loggin/loggin';
-import { UsuarioService } from '../../services/usuarios';
-
-
+import { AuthService } from '../../services/auth';
+import { RouterLink } from '@angular/router';
+import { Profile } from '../../profile/profile';
 @Component({
   selector: 'app-user-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule, Loggin],
+  imports: [RouterLink, CommonModule, Loggin, Profile],
   templateUrl: './user-navbar.html',
   styleUrls: ['./user-navbar.css']
 })
 export class UserNavbar {
+  mostrarLogin = false;
+  mostrarPerfil = false;
+  mostrarAjustes = false;
 
-  usuarioService!: UsuarioService;
+  constructor(public auth: AuthService) {}
 
-  mostrarLogin = false; 
   abrirLogin() {
     this.mostrarLogin = true;
   }
+
   cerrarLogin() {
     this.mostrarLogin = false;
   }
 
-  logueado = false;
-  login() {
-    const accesoValido = true;
-    if (accesoValido){
-      this.logueado = true;
-    }else {
-      alert('Credenciales Invalidas');
-    }
+  abrirPerfil() {
+    this.mostrarPerfil = true;
+    this.mostrarAjustes = false;
+  }
+
+  cerrarPerfil() {
+    this.mostrarPerfil = false;
+  }
+
+  toggleAjustes() {
+    this.mostrarAjustes = !this.mostrarAjustes;
+  }
+
+  logout() {
+    this.auth.logout();
+
+   
+    this.mostrarAjustes = false;
+    this.mostrarPerfil = false;
+    this.mostrarLogin = false;
   }
 }

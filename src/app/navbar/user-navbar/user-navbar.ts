be539b75dd5dbc65c5ac8b662d570ba3a1e1cd54
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-04-15 14:40:20 -0500

    cambios

--- src/app/navbar/user-navbar/user-navbar.ts
+++ src/app/navbar/user-navbar/user-navbar.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Loggin } from '../../loggin/loggin';
 import { AuthService } from '../../services/auth';
-import { RouterLink } from '@angular/router';
+
 import { Profile } from '../../profile/profile';
 @Component({
   selector: 'app-user-navbar',
--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrls: ['./profile.css']
 })
 export class Profile {
 
+  usuario: any = {};
+  citas: any[] = [];
+  foto: string = '';
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.usuario = this.auth.obtenerUsuario();
+    this.cargarCitas();
+    this.cargarFoto();
+  }
+
+  cargarCitas() {
+    const data = localStorage.getItem('citas');
+    if (data) {
+      this.citas = JSON.parse(data);
+    }
+  }
+
+  irACitas() {
+    this.router.navigate(['/citas']);
+  }
+
+  cargarFoto() {
+    this.foto = localStorage.getItem('fotoPerfil') || '';
+  }
+
+  
+  cambiarFoto(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.foto = reader.result as string;
+      localStorage.setItem('fotoPerfil', this.foto);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
 }

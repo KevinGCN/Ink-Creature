6c6b990734e841a2cadc504c2cd9d8aa707b287e
KevinGCN <cobokevingiancarlo1@gmail.com>
2026-04-16 15:49:36 -0500

    cambios

--- src/app/loggin/loggin.ts
+++ src/app/loggin/loggin.ts
@@ -82,27 +82,7 @@ export class Loggin {
     console.log('Registro correcto');
   }
 
-ngAfterViewInit(): void {
-  if (typeof window !== 'undefined' && typeof google !== 'undefined') {
 
-    google.accounts.id.initialize({
-      client_id: "TU_CLIENT_ID_DE_GOOGLE.apps.googleusercontent.com",
-      callback: (response: any) => {
-        console.log("Usuario autenticado:", response);
-      }
-    });
-
-    google.accounts.id.renderButton(
-      document.getElementById("googleLogin"),
-      { theme: "outline", size: "large", width: 250 }
-    );
-
-    google.accounts.id.renderButton(
-      document.getElementById("googleRegister"),
-      { theme: "outline", size: "large", width: 250 }
-    );
-  }
-}
 renderGoogle() {
   if (typeof window !== 'undefined' && typeof google !== 'undefined') {
 
@@ -133,5 +113,4 @@ renderGoogle() {
       });
     }
   }
-}
 }

c403d55b6e922ff66dd0fb59e742982f6ecd9465
KevinGCN <cobokevingiancarlo1@gmail.com>
2026-04-16 18:00:51 -0500

    cambios

--- src/app/loggin/loggin.spec.ts
+++ src/app/loggin/loggin.spec.ts
@@ -1,21 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Loggin } from './loggin';
-import { vi } from 'vitest';
 
 describe('Loggin', () => {
   let component: Loggin;
   let fixture: ComponentFixture<Loggin>;
 
   beforeEach(async () => {
-    // Simulación de Google API
-    (window as any).google = {
-      accounts: {
-        id: {
-          initialize: jasmine.createSpy('initialize'),
-          renderButton: jasmine.createSpy('renderButton')
-        }
-      }
-    };
 
     await TestBed.configureTestingModule({
       imports: [Loggin]

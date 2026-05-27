baa9edc30afc47496267bae1e8c4c5dec173cd57
KevinGCN <cobokevingiancarlo1@gmail.com>
2026-05-27 13:59:11 -0500

    reload emplyees view

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -49,6 +49,7 @@ export class Gallery implements OnInit {
 
   // Inicializa la galería combinando imágenes base y almacenadas
   ngOnInit() {
+    window.location.reload();
     this.auth.usuario$.subscribe(usuario => {
       this.esAdmin =
         usuario?.charge === 'CEO' ||
